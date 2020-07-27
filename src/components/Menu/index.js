a8e245152763798dc296cebcbc872fb98752865a
Paulo Lima <paulobruny@gmail.com>
2020-07-27 13:42:06 -0300

    add first component menu with logo and style

---
+++ src/components/Menu/index.js
@@ -0,0 +1,22 @@
+import React from "react"
+import Logo from "../../assets/img/pbflix.png"
+import Button from './components/Button'
+import './style.css'
+
+const Menu = () => {
+  return (
+    <nav className="Menu">
+      <a href="/">
+        <img
+          className="Logo"
+          src={Logo}
+          alt="pbflix escrito em fonte vermelha com um formato de arco enflexado para cima"
+        />
+      </a>
+
+      <Button className="ButtonLink" link="/" label="Novo Vídeo" />
+    </nav>
+  )
+}
+
+export default Menu
